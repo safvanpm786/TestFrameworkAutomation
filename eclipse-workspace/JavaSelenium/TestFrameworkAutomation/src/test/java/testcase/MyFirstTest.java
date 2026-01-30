cd12154a7e98efeea9817b5b245af8ee55b467e9
Safvanpm786 <safvanpm586@gmail.com>
2026-01-31 03:57:26 +0800

    commit changes on testng

--- eclipse-workspace/JavaSelenium/TestFrameworkAutomation/src/test/java/testcase/MyFirstTest.java
+++ eclipse-workspace/JavaSelenium/TestFrameworkAutomation/src/test/java/testcase/MyFirstTest.java
@@ -6,6 +6,7 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import base.BaseTest;
@@ -13,16 +14,16 @@ import base.BaseTest;
 
 public class MyFirstTest extends BaseTest{
 	
-	@Test
-	public static void LoginTest() throws InterruptedException {
+	@Test(dataProvider="testdata")
+	public static void LoginTest(String username,String password) throws InterruptedException {
 	driver.manage().window().maximize();
 	driver.findElement(By.linkText(loc.getProperty("signin_link"))).click();
 
 	
 	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
-	driver.findElement(By.id(loc.getProperty("email_field"))).sendKeys("safvan.pmohamed@gmail.com");
+	driver.findElement(By.id(loc.getProperty("email_field"))).sendKeys(username);
 	driver.findElement(By.xpath(loc.getProperty("next_button"))).click();
-	driver.findElement(By.id(loc.getProperty("password_field"))).sendKeys("Podapatty@586");
+	driver.findElement(By.id(loc.getProperty("password_field"))).sendKeys(password);
 	driver.findElement(By.id(loc.getProperty("login_nextbutton"))).click();
 	driver.findElement(By.xpath(loc.getProperty("click_logout"))).click();
 	
@@ -33,4 +34,14 @@ public class MyFirstTest extends BaseTest{
 	
 	}
 
+
+@DataProvider(name="testdata")
+public Object[][] tdata(){
+	return new Object[][] {
+		
+		{"safvan.pmohamed@gmail.com","Podapatty@586"},
+		{"safvan.pmohamed@gmail.com","Podapatty@586"},
+		{"safvan.pmohamed@gmail.com","Podapatty@586"}
+	};
+}
 }
